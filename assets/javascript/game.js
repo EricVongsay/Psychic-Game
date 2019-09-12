4d4d8f9fb96877f7a0b1d4cb7f0f052abcaaf9e4
Vongsy <evongsay@outlook.com>
2019-09-11 17:50:15 -0700

    basic layout of javascript and pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,45 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 for(var i =0;i<letters.length;i++){
 
 }
+
+
 // randomize choice
 var computerChoice = letters[Math.floor(Math.random()*letters.length)];
-// get user guess through onkey
-var userGuess=event.key;
+console.log(computerChoice);
 
-// create variables for score
+
+// get user guess through onkey
+document.onkeyup= function(event){
+
+
+    var userGuess=event.key;
+    
+
+    // create variables for score
+    var wins=0;
+    var losses=0;
+    var chances=9;
+
+    var winText= document.getElementById("wins");
+    var lossesText=document.getElementById("losses");
+    var chancesText=document.getElementById("guesses");
+    var guessText=document.getElementById("yourGuess");
+
+    // create if statement to increase score
+    if (userGuess === computerChoice){
+        wins++;
+    }else{
+        losses++;
+        guesses--;
+    };
+
+    //creating array for guesses
+    var guesses=[];
+    var newDiv=document.createElement("div");
+    guessText.appendChild(newDiv);
+    guesses.push(userGuess);
+
+};
+
+
+
